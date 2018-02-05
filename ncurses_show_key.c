71cbb1b7641b95e425fce2df0774ffd19178bfea
skamsie <alexandru.dimian@gmail.com>
2018-02-05 23:58:12 +0100

    ncurses show key

---
+++ ncurses_show_key.c
@@ -0,0 +1,28 @@
+// show ncurses code for pressed key
+
+#include <stdio.h>
+#include <ncurses.h>
+
+int main()
+{
+    int c;
+    initscr();
+    curs_set(0);
+    noecho();
+    cbreak();
+
+    mvprintw(24, 0, "Type any key. ESC exits.");
+
+    refresh();
+
+    // white key is different from ESC
+    while((c = getch()) != 27) {
+        mvprintw(24, 0, "Character pressed is = %3d. Hopefully it can be printed as '%c'", c, c);
+        refresh();
+    }
+
+    clrtoeol();
+    refresh();
+    endwin();
+    return 0;
+}
